dd80c142403b381e9bc340716ea3380af1480de1
johnny-keker <giomail.iv@gmail.com>
2020-09-16 00:50:30 +0300

    [testing]<lab1> implemented sin function and wrote some tests

--- testing/Lab1/src/main/scala/Function.scala
+++ testing/Lab1/src/main/scala/Function.scala
@@ -1,3 +1,18 @@
 object Function {
-  def sin(x: Double): Double = Math.sin(x)
+  val PRECISION = 1E-8
+
+  def sin(x: Double): Double = x match {
+    case 0.0 => 0.0
+    case _ => s(x, x)
+  }
+
+  def s(x: Double, cur: Double, n: Int = 1, res: Double = 0.0): Double = {
+    if (Math.abs(cur) < PRECISION / 10) return res
+    s(x, Math.pow(-1, n) * (Math.pow(x, 2 * n + 1) / factorial(2 * n + 1)), n + 1, res + cur)
+  }
+
+  def factorial(x: Int, res: Int = 1): Double = x match {
+    case 1 => res
+    case _ => factorial(x - 1, res * x)
+  }
 }
--- testing/Lab1/src/test/scala/FunctionTests.scala
+++ testing/Lab1/src/test/scala/FunctionTests.scala
@@ -1,9 +1,16 @@
 import org.junit.Test
 import org.junit.Assert._
+import scala.util.Random
 
 class FunctionTests {
   @Test
-  def testSample() = {
+  def testConst() = {
     assertEquals(0.0, Function.sin(0.0), 0)
+  }
+
+  @Test
+  def testRandom() = {
+    val x = Random.nextDouble()
+    assertEquals(Math.sin(x), Function.sin(x), Function.PRECISION)
   }
 }
