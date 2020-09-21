281eac080a9f54672d9f1e09244b7c4f2487f3ff
johnny-keker <giomail.iv@gmail.com>
2020-09-21 13:34:26 +0300

    [testing]<lab1> sin changed formula and added ox intersection and extr test

--- testing/Lab1/src/main/scala/Sinus.scala
+++ testing/Lab1/src/main/scala/Sinus.scala
@@ -1,6 +1,7 @@
+import scala.annotation.tailrec
 
 object Sinus {
-  val PRECISION = 1E-10
+  val PRECISION = 1E-6
 
   def compute(x: Double): Double = if (x.isNaN) Double.NaN else x match {
     case Double.PositiveInfinity => Double.NaN
@@ -8,9 +9,10 @@ object Sinus {
     case _ => s(x, x)
   }
 
+  @tailrec
   def s(x: Double, cur: Double, n: Int = 1, res: Double = 0.0): Double = {
     if (Math.abs(cur) < PRECISION) return res
-    s(x, Math.pow(-1, n) * (Math.pow(x, 2 * n + 1) / factorial(2 * n + 1)), n + 1, res + cur)
+    s(x, cur * (-x * x / (2.0 * n * (2.0 * n + 1.0))), n + 1, res + cur)
   }
 
   def factorial(x: Int, res: Int = 1): Double = x match {
--- testing/Lab1/src/test/scala/SinusTests.scala
+++ testing/Lab1/src/test/scala/SinusTests.scala
@@ -1,19 +1,31 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions._
+
 import scala.util.Random
 
 class SinusTests {
-  /*
-  @Test
-  def testConst() = {
-    assertEquals(0.0, Sinus.compute(0.0), "This is sample test - sin(0.0)")
-  }
-  */
   @Test
   def testArgsValidation(): Unit = {
     assertEquals(Double.NaN, Sinus.compute(Double.NaN), "sin(NaN) must be NaN")
     assertEquals(Double.NaN, Sinus.compute(Double.PositiveInfinity), "sin(+inf) must be NaN")
     assertEquals(Double.NaN, Sinus.compute(Double.NegativeInfinity), "sin(-inf) must be NaN")
+  }
+
+  @Test
+  def oxIntersectionPointsTest(): Unit = {
+    for (i <- 0 to 5) {
+      assertEquals(0.0, Sinus.compute(Math.PI * i.toDouble), Sinus.PRECISION, s"sin(PI*$i) must be 0.0")
+      assertEquals(0.0, Sinus.compute(-Math.PI * i.toDouble), Sinus.PRECISION, s"sin(-PI*$i) must be 0.0")
+    }
+  }
+
+  @Test
+  def extremumPointsTest(): Unit = {
+    var expected = 1.0
+    for (i <- 1 to 7 by 2) {
+      assertEquals(expected, Sinus.compute(Math.PI / 2.0 * i.toDouble), Sinus.PRECISION, s"sin(PI/2*$i) must be $expected")
+      expected *= -1
+    }
   }
 
   @Test
