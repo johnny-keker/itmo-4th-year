ba6251e55834f24dbc8ef7d2265c0d3f50f26a1f
johnny-keker <giomail.iv@gmail.com>
2020-09-21 12:52:59 +0300

    [testing]<lab1> sin validate arg

--- testing/Lab1/src/main/scala/Sinus.scala
+++ testing/Lab1/src/main/scala/Sinus.scala
@@ -1,14 +1,15 @@
 
 object Sinus {
-  val PRECISION = 1E-8
+  val PRECISION = 1E-10
 
-  def compute(x: Double): Double = x match {
-    case 0.0 => 0.0
+  def compute(x: Double): Double = if (x.isNaN) Double.NaN else x match {
+    case Double.PositiveInfinity => Double.NaN
+    case Double.NegativeInfinity => Double.NaN
     case _ => s(x, x)
   }
 
   def s(x: Double, cur: Double, n: Int = 1, res: Double = 0.0): Double = {
-    if (Math.abs(cur) < PRECISION / 10) return res
+    if (Math.abs(cur) < PRECISION) return res
     s(x, Math.pow(-1, n) * (Math.pow(x, 2 * n + 1) / factorial(2 * n + 1)), n + 1, res + cur)
   }
 
--- testing/Lab1/src/test/scala/SinusTests.scala
+++ testing/Lab1/src/test/scala/SinusTests.scala
@@ -3,9 +3,17 @@ import org.junit.jupiter.api.Assertions._
 import scala.util.Random
 
 class SinusTests {
+  /*
   @Test
   def testConst() = {
     assertEquals(0.0, Sinus.compute(0.0), "This is sample test - sin(0.0)")
+  }
+  */
+  @Test
+  def testArgsValidation(): Unit = {
+    assertEquals(Double.NaN, Sinus.compute(Double.NaN), "sin(NaN) must be NaN")
+    assertEquals(Double.NaN, Sinus.compute(Double.PositiveInfinity), "sin(+inf) must be NaN")
+    assertEquals(Double.NaN, Sinus.compute(Double.NegativeInfinity), "sin(-inf) must be NaN")
   }
 
   @Test
