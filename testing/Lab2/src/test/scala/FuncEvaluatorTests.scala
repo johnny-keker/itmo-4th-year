065581448d34b3ebdbd6d4e06f0d6609a7dcb899
johnny-keker <giomail.iv@gmail.com>
2020-10-04 21:02:12 +0300

    [testing]<lab2> positive area basic test

--- testing/Lab2/src/test/scala/FuncEvaluatorTests.scala
+++ testing/Lab2/src/test/scala/FuncEvaluatorTests.scala
@@ -9,7 +9,7 @@ class FuncEvaluatorTests extends AnyFunSuite with MockFactory {
   val epsilon = 1E-6
 
   implicit val doubleEq: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(epsilon)
-  
+
   test("negative area basic test") {
     val mockTrig = mock[TTrigEvaluator]
     (mockTrig.sin _).expects(-0.313, 1E-6).returning(Math.sin(-0.313)).repeated(2).times()
@@ -17,8 +17,25 @@ class FuncEvaluatorTests extends AnyFunSuite with MockFactory {
     (mockTrig.csc _).expects(-0.313, 1E-6).returning(1 / Math.sin(-0.313)).repeated(2).times()
 
     val mockLog = mock[TLogEvaluator]
+    (mockLog.log _).expects(*, *, *).never()
 
     val f = new FuncEvaluator(mockTrig, mockLog)
     assert(f.func(-0.313) === -0.706703, "sample negative area test with trig evaluator mocking")
+  }
+
+  test("positive area basic test") {
+    val mockTrig = mock[TTrigEvaluator]
+    (mockTrig.sin _).expects(*, *).never()
+    (mockTrig.cos _).expects(*, *).never()
+    (mockTrig.csc _).expects(*, *).never()
+
+    val mockLog = mock[TLogEvaluator]
+    (mockLog.log _).expects(0.313, 2, 1E-6).returning(Math.log(0.313) / Math.log(2)).once()
+    (mockLog.log _).expects(0.313, 10, 1E-6).returning(Math.log(0.313) / Math.log(10)).once()
+    (mockLog.log _).expects(0.313, 5, 1E-6).returning(Math.log(0.313) / Math.log(5)).repeated(2).times()
+    (mockLog.log _).expects(0.313, 3, 1E-6).returning(Math.log(0.313) / Math.log(3)).repeated(3).times()
+
+    val f = new FuncEvaluator(mockTrig, mockLog)
+    assert(f.func(0.313) === 0.403459, "sample positive area test with trig evaluator mocking")
   }
 }
