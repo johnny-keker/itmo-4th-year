118bcfbeaa903594b665dfd654d7ce8e00a496ff
johnny-keker <giomail.iv@gmail.com>
2020-10-16 20:59:41 +0300

    [testing]<lab3> added sample query test

--- testing/Lab3/src/test/kotlin/SampleSeleniumTests.kt
+++ testing/Lab3/src/test/kotlin/SampleSeleniumTests.kt
@@ -3,9 +3,19 @@ import webpages.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 
 class SampleSeleniumTests {
+    private val queryResultsXPath = "(//table[@class='t_peer w100p']/tbody/tr[@class='first bg' or @class='bg']/td[@class='nam'])"
+    private val sampleQueryResults = arrayOf(
+        "Black Sabbath - Paranoid / Rock / 2009 / MP3",
+        "Classic Albums - Black Sabbath - Paranoid / 2010 / СТ / BDRip (720p)",
+        "Black Sabbath - Paranoid (Deluxe Edition, 2CD) (1970) / Rock / 2016 / MP3",
+        "Black Sabbath - Paranoid (Deluxe Edition) (1970) / Rock / 2016 / FLAC / Lossless",
+        "Black Sabbath - Paranoid In The 70's / 2007 / БП / DVD-5"
+    )
+
     @ParameterizedTest
     @ArgumentsSource(DriverProvider::class)
     fun `sample test`(driver: WebDriver) {
@@ -32,5 +42,21 @@ class SampleSeleniumTests {
         assertEquals(Credentials.login, mainPage.username.text)
 
         driver.quit()
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(DriverProvider::class)
+    fun `sample query test`(driver: WebDriver) {
+        val mainPage = MainPage(driver)
+        assertTrue(mainPage.searchInput.isDisplayed)
+        assertTrue(mainPage.searchButton.isDisplayed)
+
+        mainPage.searchInput.sendKeys("black sabbath paranoid")
+        mainPage.searchButton.click()
+
+        for (i in 0..4) {
+            val queryRes = driver.findElement(By.xpath("$queryResultsXPath[${i+1}]")).text
+            assertEquals(sampleQueryResults[i], queryRes)
+        }
     }
 }
--- testing/Lab3/src/test/kotlin/webpages/MainPage.kt
+++ testing/Lab3/src/test/kotlin/webpages/MainPage.kt
@@ -21,6 +21,16 @@ class MainPage(private val driver: WebDriver) {
     @FindBy(xpath = "//li[@class='tp2 center b']/a[@class='u0']")
     lateinit var username: WebElement
 
+    @FindBy(xpath = "")
+    lateinit var queryResult: WebElement
+
+    @FindBy(xpath = "//input[@name='s']")
+    lateinit var searchInput: WebElement
+
+    @FindBy(xpath = "//input[@class='s_submit']")
+    lateinit var searchButton: WebElement
+
+
     init {
         driver.get("http://kinozal.tv/")
         PageFactory.initElements(driver, this)
