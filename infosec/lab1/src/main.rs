831c47831b454827195cdc275553c33a96e02545
johnny-keker <isarzhevsky@gmail.com>
2021-02-27 14:38:44 +0300

    [infosec]<lab1> refactor, arbitrary offset

--- infosec/lab1/src/main.rs
+++ infosec/lab1/src/main.rs
@@ -8,22 +8,32 @@ const ALPHABET: [char; 26] = [
     'v', 'w', 'x', 'y', 'z'
 ];
 
+fn encode_text(input_text: String, offset: usize) -> String {
+    let len = input_text.len();
+    let mut result = String::with_capacity(len);
+
+    for cur_char in input_text.chars() {
+        let lower_case = cur_char.to_ascii_lowercase();
+        let idx = ALPHABET.iter().position(|&r| r == lower_case).unwrap_or(27);
+
+        let new_char = if idx == 27 {lower_case} else {ALPHABET[(idx + offset) % 26]};
+        
+        result.push(new_char);
+    };
+
+    return result;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
+    let offset = *(&args[2].parse::<usize>().unwrap());
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     
     println!("Source text:\n{}", contents);
 
     println!("Encoded text:");
-    for cur_char in contents.chars() {
-        let lower_case = cur_char.to_ascii_lowercase();
-        let idx = ALPHABET.iter().position(|&r| r == lower_case).unwrap_or(27);
-
-        let new_char = if idx == 27 {lower_case} else {ALPHABET[(idx + 2) % 26]};
-        
-        print!("{}", new_char);
-    }
+    println!("{}", encode_text(contents, offset));
 }
